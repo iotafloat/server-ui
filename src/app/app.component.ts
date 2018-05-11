10907671d8369571232f047b3c18647779d72ed1
Margarita Tagaban <v-mataga@microsoft.com>
2018-05-11 13:17:18 -0700

    Wrapped titles of each card in h3 tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  panelOpenState: boolean = false;
+  panelOpenState: false;
 }
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-}
+export class DashboardComponent {}

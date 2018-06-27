f4b2df6e42d00ba5d067315a3b787a72b1fc3839
Jeremy Foster <jeremy.foster@live.com>
2018-06-27 14:11:59 -0500

    animate the diagram

--- src/app/vessel-diagram/vessel-diagram.component.ts
+++ src/app/vessel-diagram/vessel-diagram.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'vessel-diagram',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vessel-diagram.component.css']
 })
 export class VesselDiagramComponent implements OnInit {
+  isAlert: boolean = true;
 
-  constructor() { }
+  public get currentClasses(): {} {
+    return {
+      alert: this.isAlert
+    }
+  }
+
+  constructor(private host: ElementRef) { }
 
   ngOnInit() {
+    let steps = [
+      () => {
+        this.host.nativeElement.querySelector('#devices #rpz-masthead').classList.add('good');
+      },
+      () => {
+        this.host.nativeElement.querySelector('#devices #rpz-master').classList.add('good');
+      },
+      () => {
+        this.host.nativeElement.querySelector('#devices #rpz-cockpit').classList.add('good');
+        this.host.nativeElement.querySelector('#zones #galley').classList.add('alert');
+      },
+      () => {
+        this.host.nativeElement.querySelector('#devices #rpz-aftport').classList.add('good');
+      },
+      () => {
+        this.host.nativeElement.querySelector('#devices #rpz-aftstar').classList.add('good');
+      },
+      () => {
+        this.host.nativeElement.querySelector('#devices #rpz-cabin').classList.add('good');
+        this.host.nativeElement.querySelector('#zones #galley').classList.add('good');
+      },
+      () => {
+        this.host.nativeElement.querySelector('#devices #rpz-engine').classList.add('alert');
+        this.host.nativeElement.querySelector('#zones #galley').classList.remove('good');
+      },
+    ];
+
+    let i = 0;
+    setInterval(() => {
+          steps[i]();
+          i = ((i < steps.length - 1) ? i + 1 : 0 ); //increment or restart
+    }, 1000);
+
+  }
+
+  delay(s) {
+    return new Promise(resolve => setTimeout(resolve, s * 1000));
   }
 
 }
